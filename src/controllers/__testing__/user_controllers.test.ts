186433d5143b5fe58e105902b122b1dc0b5c3b63
Bensagra <bensagra@gmail.com>
2024-12-06 11:42:25 -0300

    test: enhance user login tests with JWT mocking and clear mocks before each test

--- src/controllers/__testing__/user_controllers.test.ts
+++ src/controllers/__testing__/user_controllers.test.ts
@@ -3,24 +3,34 @@ import { prismaMock } from "../../singelton";
 import { jest } from "@jest/globals";
 import * as jwtControllers from "../../utilities/jwt";
 
-
 jest.mock("../../utilities/emailVerification");
 jest.mock("../../utilities/passwordReset");
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("userControllers.login", () => {
   it("debería autenticar a un usuario correctamente", async () => {
+    // Mock de request y response
     const req = { body: { email: "test@example.com", password: "password123" } };
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
 
+    // Mock de utilidades de JWT
+    const encryptedPassword = jwtControllers.encryptPassword("password123");
+    jest.spyOn(jwtControllers, "encryptPassword").mockReturnValue(encryptedPassword);
+    jest.spyOn(jwtControllers, "decryptPassword").mockReturnValue("password123");
+
+    // Mock de Prisma
     prismaMock.user.findUnique.mockResolvedValue({
       id: 1,
       name: "Test",
       surname: "User",
       email: "test@example.com",
-      password: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6InBhc3N3b3JkMTIzIiwiaWF0IjoxNzMzNDU0ODQ1fQ.ag6cwibi1yaECmqA-KVbn-TzhSsnz82__LRtLvLoSFw",
+      password: encryptedPassword,
       address: "123 Test St",
       phone: "123-456-7890",
       createdAt: new Date(),
@@ -29,10 +39,10 @@ describe("userControllers.login", () => {
       verified: true,
     });
 
-    jest.spyOn(jwtControllers, "decryptPassword").mockReturnValue("password123");
-
+    // Llamada al controlador
     await userControllers.login(req, res);
 
+    // Verifica que el controlador se comporta como se espera
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({ message: "Usuario logueado" });
   });

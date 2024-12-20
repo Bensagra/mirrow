1a6ad76726ea85270bcb0e53e0f9770edcfaa50b
Bensagra <bensagra@gmail.com>
2024-12-20 09:21:55 -0300

    feat: add clothes controller and route to fetch clothing items

---
+++ src/controllers/product_controllers.ts
@@ -0,0 +1,52 @@
+import prisma from "../client";
+
+const getClothes = async (req: any, res: any) => {
+    try {
+        const products = await prisma.clothes.findMany({
+           
+    include: {
+        subType:{
+
+            include: {
+                type: true
+        },},
+        sizeStocks: {
+            include: {
+
+                size: true
+            }
+        },
+    },
+    });
+
+   let finalClothes = products.map((product: any) => {
+        return {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            description: product.description,
+            image: product.image,
+            subType: product.subType.name,
+            type: product.subType.type.name,
+            fabric: product.fabric,
+            color: product.color,
+            sizeStocks: product.sizeStocks.map((sizeStock: any) => {
+                return{
+                    size: sizeStock.size.name,
+                    stock: sizeStock.stock,
+                    sizeId: sizeStock.sizeId
+                }
+        })
+        }
+    });
+        return res.status(200).json(finalClothes);
+    } catch (error) {
+        console.error("Error en getProducts:", error);
+        return res.status(500).json({ message: "Error en el servidor" });
+    }
+}
+
+
+export const clothesControllers = {
+   getClothes,
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {userControllers}  from "./controllers/user_controllers";
+import { clothesControllers } from "./controllers/product_controllers";
 
 
 
@@ -12,4 +13,7 @@ router.post("/users/login",(req,res)=>userControllers.login(req,res));
 router.put("/users/verify",(req,res)=>userControllers.verify(req,res));
 router.post("/users/requestPasswordReset",(req,res)=>userControllers.requestPasswordReset(req,res));
 router.put("/users/resetPassword",(req,res)=>userControllers.resetPassword(req,res));
+
+
+router.get("/clothes",(req,res)=>clothesControllers.getClothes(req,res));
 export default router;

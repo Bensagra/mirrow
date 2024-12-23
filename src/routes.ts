e9b32f5a77f62a5193fb293fdb73a79566ea456e
Bensagra <Bensagra@gmail.com>
2024-12-22 23:43:58 -0300

    feat: add createClothes route and improve type annotations in routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,36 @@
-import { Router } from "express";
-import {userControllers}  from "./controllers/user_controllers";
+import { Router, Request, Response } from "express";
+import { userControllers } from "./controllers/user_controllers";
 import { clothesControllers } from "./controllers/product_controllers";
 import prisma from "../src/client";
 
-
-
-
-
 const router = Router();
 
-router.post("/users/register",(req,res)=>userControllers.register(req,res,prisma));
-router.post("/users/login",(req,res)=>userControllers.login(req,res,prisma));
-router.put("/users/verify",(req,res)=>userControllers.verify(req,res,prisma));
-router.post("/users/requestPasswordReset",(req,res)=>userControllers.requestPasswordReset(req,res,prisma));
-router.put("/users/resetPassword",(req,res)=>userControllers.resetPassword(req,res,prisma));
+// Rutas para usuarios
+router.post("/users/register", (req: Request, res: Response) =>
+    userControllers.register(req, res, prisma)
+);
+router.post("/users/login", (req: Request, res: Response) =>
+    userControllers.login(req, res, prisma)
+);
+router.put("/users/verify", (req: Request, res: Response) =>
+    userControllers.verify(req, res, prisma)
+);
+router.post("/users/requestPasswordReset", (req: Request, res: Response) =>
+    userControllers.requestPasswordReset(req, res, prisma)
+);
+router.put("/users/resetPassword", (req: Request, res: Response) =>
+    userControllers.resetPassword(req, res, prisma)
+);
 
+// Rutas para ropa
+router.get("/clothes", (req: Request, res: Response) =>
+    clothesControllers.getClothes(req, res, prisma)
+);
+router.put("/clothes/updateStock", (req: Request, res: Response) =>
+    clothesControllers.updateStock(req, res, prisma)
+);
+router.post("/clothes/create", (req: Request, res: Response) =>
+    clothesControllers.createClothes(req, res, prisma)
+);
 
-router.get("/clothes",(req,res)=>clothesControllers.getClothes(req,res, prisma));
-router.put("/clothes/updateStock",(req,res)=>clothesControllers.updateStock(req,res, prisma));
 export default router;
